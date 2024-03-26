77d2e9339fa659f8c44236f58f4c656c33f4703c
tracehook <trace.hook10@gmail.com>
2024-03-26 13:21:30 -0500

    Pathfinding Algo

--- Board.java
+++ Board.java
@@ -1,16 +1,14 @@
 public class Board {
-
     private Block[][] gameBoard;
 
     public Board(byte width, byte height) {
         this.gameBoard = new Block[width][height];
         initiateBlankBoard();
-
     }
 
     private void initiateBlankBoard() {
         for (byte x = 0; x < gameBoard.length; x++) {
-            for (byte y = 0; y < gameBoard.length; y++) {
+            for (byte y = 0; y < gameBoard[x].length; y++) {
                 gameBoard[x][y] = new Block();
             }
         }
@@ -18,5 +16,16 @@ public class Board {
 
     public Block getBoardBlock(byte[] position) {
         return this.gameBoard[position[0]][position[1]];
+    }
+
+    public int getWidth() {
+        return gameBoard.length;
+    }
+
+    public int getHeight() {
+        if (gameBoard.length == 0) {
+            return 0;
+        }
+        return gameBoard[0].length;
     }
 }
